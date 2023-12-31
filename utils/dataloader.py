4f57297c607d0020679f6c60040305af21b19c03
Stephan <s.weissbach@uni-mainz.de>
2023-12-31 17:34:01 +0100

    update of dataloader to make training on active patches possible

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -16,6 +16,8 @@ class DataLoader:
         train_width: int,
         load_multiple_targets_per_file: bool = False,
         n_multiple_targets: int = 5,
+        use_active_frames: bool = True,
+        crop_size: int = 16,
     ):
         """
         Initialize the DataLoader.
@@ -47,6 +49,8 @@ class DataLoader:
         self.train_height = train_height
         self.train_width = train_width
         self.load_multiple_targets_per_file = load_multiple_targets_per_file
+        self.use_active_frames = use_active_frames
+        self.crop_size = crop_size
         self.epoch_done = False
         if self.load_multiple_targets_per_file:
             self.n_multiple_targets = n_multiple_targets
@@ -57,16 +61,24 @@ class DataLoader:
         for iglu_movie in self.file_dict.keys():
             path = self.file_dict[iglu_movie]["filepath"]
             movie_len = self.file_dict[iglu_movie]["shape"][0]
+            active_frames = self.file_dict[iglu_movie]["frames_and_positions"]
             if self.load_multiple_targets_per_file:
                 iterate_to = movie_len - self.n_post - 1 - n_multiple_targets
                 iterate_step = self.n_multiple_targets
             else:
                 iterate_to = movie_len - self.n_post - 1
                 iterate_step = 1
-            self.train_examples += [
-                [path, target]
-                for target in range(self.n_pre + 1, iterate_to, iterate_step)
-            ]
+            if use_active_frames:
+                self.train_examples += [
+                    [path, frame[0], frame[1], frame[2]]
+                    for frame in active_frames
+                    if (frame[0] > self.n_pre and frame[0] <= iterate_to)
+                ]
+            else:
+                self.train_examples += [
+                    [path, target, np.nan, np.nan]
+                    for target in range(self.n_pre + 1, iterate_to, iterate_step)
+                ]
             self.norm_vals[path] = {
                 "mean": self.file_dict[iglu_movie]["mean"],
                 "std": self.file_dict[iglu_movie]["std"],
@@ -86,15 +98,24 @@ class DataLoader:
             self.train_examples[idx] for idx in random_order
         ]
 
-    def crop_pad_img(self, img: np.ndarray) -> np.ndarray:
+    def crop_pad_img(
+        self, img: np.ndarray, y_min: int = -1, x_min: int = -1
+    ) -> np.ndarray:
         """
         Shuffle the training examples for a new epoch.
         """
+        # in case of training on small patches, cropping will work for sure (as it is insured
+        # by trainfiles.py). So it can directly return the croped images
+        if self.use_active_frames:
+            img = img[
+                :, y_min : y_min + self.train_height, x_min : x_min + self.train_width
+            ]
+            return img
+        # else the input image is cropped/padded
         _, height, width = img.shape
         diff_height = self.train_height - height
         diff_width = self.train_width - width
         pad = False
-
         # crop
         if diff_height < 0:
             start = np.random.choice(abs(diff_height))
@@ -104,7 +125,6 @@ class DataLoader:
             start = np.random.choice(abs(diff_width))
             stop = width - (abs(diff_width) - start)
             img = img[:, :, start:stop]
-
         # pad
         if diff_height > 0:
             pad = True
@@ -130,7 +150,13 @@ class DataLoader:
         return img
 
     def create_train_example(
-        self, filepath: str, target: int, mean: float, std: float
+        self,
+        filepath: str,
+        target: int,
+        mean: float,
+        std: float,
+        y_min: int,
+        x_min: int,
     ) -> None:
         """
         Create a training example using the specified file, target, mean, and std.
@@ -142,7 +168,7 @@ class DataLoader:
         - std: Standard deviation for scaling.
         """
         img = tifffile.imread(filepath)
-        img = self.crop_pad_img(img)
+        img = self.crop_pad_img(img, y_min, x_min)
         for i in range(self.n_multiple_targets):
             target_ = target + i
             # Extract frames, scale, and copy middle frame to y and delete
@@ -170,10 +196,11 @@ class DataLoader:
                 # Check if available_train_examples is empty, indicating the end of the epoch
                 self.epoch_done = True
                 return False
-            filepath, target = self.available_train_examples.pop(0)
+            filepath, target, y_min, x_min = self.available_train_examples.pop(0)
             mean = self.norm_vals[filepath]["mean"]
             std = self.norm_vals[filepath]["std"]
-            self.create_train_example(filepath, target, mean, std)
+            self.create_train_example(filepath, target, mean, std, y_min, x_min)
+
         self.X = torch.tensor(np.array(self.X_list), dtype=torch.float)
         del self.X_list
         self.y = torch.tensor(np.array(self.y_list), dtype=torch.float)
