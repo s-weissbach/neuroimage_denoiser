1086ec38c3cf688e6d3a9d89c8e97fee051d50b3
Stephan <s.weissbach@uni-mainz.de>
2023-12-31 16:51:57 +0100

    rolling window normalization for detection of activity

--- utils/normalization.py
+++ utils/normalization.py
@@ -19,6 +19,23 @@ def z_norm(
     return np.divide(np.subtract(img, mean), std)
 
 
+def rolling_window_z_norm(
+    img: np.ndarray,
+    window_size: int,
+) -> np.ndarray:
+    frames_before = window_size // 2
+    frames_after = window_size - frames_before
+    z_scaled_img = []
+    for idx_frame in range(img.shape[0]):
+        start = max(0, idx_frame - frames_before)
+        end = min(img.shape[0] - 1, idx_frame + frames_after + 1)
+        img_current_window = img[start:end]
+        mean = np.mean(img_current_window)
+        std = np.std(img_current_window)
+        z_scaled_img.append(z_norm(img[idx_frame], mean, std))
+    return np.array(z_scaled_img)
+
+
 def reverse_z_norm(
     img: np.ndarray, mean: Union[float, np.float64], std: Union[float, np.float64]
 ) -> np.ndarray:
