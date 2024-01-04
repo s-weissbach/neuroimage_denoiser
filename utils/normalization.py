cf1e7db6e961fba72dcf363030a6cba7e4bf835d
Stephan <s.weissbach@uni-mainz.de>
2024-01-04 16:03:43 +0100

    docstrings

--- utils/normalization.py
+++ utils/normalization.py
@@ -8,12 +8,12 @@ def z_norm(
     img: np.ndarray, mean: np.ndarray, std: np.ndarray
 ) -> np.ndarray:
     """
-    Z-scaling for the image. z = (x-µ)/σ
+    Pixelwise z-scaling for the image. z = (x-µ)/σ
 
     Parameters:
     - img: Input image.
-    - mean: Mean value for scaling.
-    - std: Standard deviation for scaling.
+    - mean: Mean matrix for scaling
+    - std: Standard deviation matrix for scaling.
 
     Returns:
     - z-scaled image.
@@ -21,18 +21,48 @@ def z_norm(
     return np.divide(np.subtract(img, mean), std)
 
 
-
 def moving_mean(parameters: tuple[int]) -> np.ndarray:
+    """
+    Calculate the moving mean for a specified range of frames.
+
+    Parameters:
+    - parameters: Tuple containing the start and end indices.
+
+    Returns:
+    - np.ndarray: Moving mean values.
+    """
     start,end = parameters
     mean = np.mean(shared_array[start:end],axis=0)
     return mean
 
+
 def moving_std(parameters: tuple[int]) -> np.ndarray:
+    """
+    Calculate the moving standard deviation for a specified range of frames.
+
+    Parameters:
+    - parameters: Tuple containing the start and end indices.
+
+    Returns:
+    - np.ndarray: Moving standard deviation values.
+    """
     start,end = parameters
     std = np.std(shared_array[start:end],axis=0)
     return std
 
+
 def rolling_window_z_norm(img: np.ndarray, window_size: int, n_threads: int = 1):
+    """
+    Apply rolling window z-scaling to an image sequence using multiprocessing.
+
+    Parameters:
+    - img: Input image sequence.
+    - window_size: Size of the rolling window.
+    - n_threads: Number of threads for parallel processing (default is 1).
+
+    Returns:
+    - np.ndarray: Z-scaled image sequence.
+    """
     # make img a shared array for multiprocessing
     shared_array_base = Array(ctypes.c_double, img.shape[0]*img.shape[1]*img.shape[2])
     global shared_array
@@ -55,6 +85,7 @@ def rolling_window_z_norm(img: np.ndarray, window_size: int, n_threads: int = 1)
     normed_img = np.divide(np.subtract(img, mean), std)
     return normed_img
 
+
 def reverse_z_norm(
     img: np.ndarray, mean: np.ndarray, std: np.ndarray
 ) -> np.ndarray:
@@ -63,8 +94,8 @@ def reverse_z_norm(
 
     Parameters:
     - img: Input image.
-    - mean: Mean value for scaling.
-    - std: Standard deviation for scaling.
+    - mean: Mean matrix for scaling.
+    - std: Standard deviation matrix for scaling.
 
     Returns:
     - reversed z-scored image.
--- utils/open_file.py
+++ utils/open_file.py
@@ -3,6 +3,22 @@ import tifffile
 import nd2
 
 def open_file(filepath: str) -> np.ndarray:
+    """
+    Open and read an image file from the specified filepath.
+
+    Parameters:
+    - filepath (str): Path to the image file.
+
+    Returns:
+    - np.ndarray: NumPy array representing the image data.
+
+    Raises:
+    - NotImplementedError: If the file format is not supported.
+
+    Supported file formats:
+    - .nd2 (using nd2reader library)
+    - .tif, .tiff, .stk (using tifffile library)
+    """
     tiff_fileendings = ['.tif', '.tiff', '.stk']
     if filepath.endswith('nd2'):
         return nd2.imread(filepath)
