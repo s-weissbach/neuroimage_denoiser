e1677e79068b94ed002d442fb0476785618c2050
Stephan <s.weissbach@uni-mainz.de>
2024-01-04 18:20:55 +0100

    routine to plot example image during training

--- model/train.py
+++ model/train.py
@@ -1,10 +1,14 @@
 from model.unet import UNet
 from utils.dataloader import DataLoader
+from utils.open_file import open_file
+import utils.normalization as normalization
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
+import os
+import matplotlib.pyplot as plt
 
 
 def train(
@@ -14,6 +18,8 @@ def train(
     learningrate: float = 0.0001,
     modelpath: str = "unet.pt",
     history_savepath: str = "train_loss.npy",
+    example_img_path: str = "",
+    predict_example_every_n_batches: int = 100
 ) -> None:
     """
     Train the U-Net model using the specified data loader.
@@ -25,7 +31,19 @@ def train(
     - learningrate (float): Learning rate for the optimizer (default is 0.0001).
     - modelpath (str): Filepath to save the trained model (default is "unet.pt").
     - history_savepath (str): Filepath to save the training loss history (default is "train_loss.npy").
+    - example_img_path (str): Path to an example image for periodic model predictions (default is "").
+    - predict_example_every_n_batches (int): Interval for making model predictions using the example image (default is 100).
     """
+    if example_img_path != "":
+        os.makedirs('example',exist_ok=True)
+        example_img = open_file(example_img_path)
+        mean = example_img.mean(axis=0)
+        std = example_img.std(axis=0)
+        example_img = normalization.z_norm(
+                example_img[0 : dataloader.n_pre + dataloader.n_post+2], mean, std
+            )
+        example_img = np.delete(example_img, dataloader.n_pre, axis=0).reshape(1,example_img.shape[0],example_img.shape[1],example_img.shape[2])
+        example_img = torch.tensor(example_img,dtype=torch.float)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
     optimizer = optim.Adam(model.parameters(), lr=learningrate)
@@ -51,6 +69,21 @@ def train(
                 print(
                     f"Batch {i+1} (samples {(i+1)*dataloader.batch_size}), Loss: {loss.item()}"
                 )
+            if i % predict_example_every_n_batches == 0 and example_img_path != '':
+                example_img.to(device)
+                model.eval()
+                prediction = np.array(model(example_img).detach().to('cpu'))
+                prediction = prediction.reshape(prediction.shape[-2],prediction.shape[-1])
+                fig,ax = plt.subplots()
+                fig.set_size_inches(5,5)
+                ax.imshow(prediction, 'Greys')
+                for orientation in ['top','bottom','left','right']:
+                    ax.spines[orientation].set_visible(False)
+                ax.set_xticks([])
+                ax.set_yticks([])
+                plt.savefig(f'example/model_prediction_{i}-batch.png')
+                plt.clf()
+                example_img.to('cpu')
             i += 1
         dataloader.shuffle_array()
     history = np.array(history)
