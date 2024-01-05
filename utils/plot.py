554b805600be875d7cf7d83eb73a2bdcf8a58e98
Stephan <s.weissbach@uni-mainz.de>
2024-01-05 19:37:26 +0100

    plot a frame (routine used during training - to see progress)

---
+++ utils/plot.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import torch
+import numpy as np
+
+
+def plot_img(
+    img: np.ndarray, savepath: str = "", vmin: float = -np.inf, vmax: float = np.inf
+) -> None:
+    fig, ax = plt.subplots()
+    fig.set_size_inches(5, 5)
+    if vmin != -np.inf:
+        vmin = vmin
+    else:
+        vmin = np.min(img)
+    if vmax != np.inf:
+        vmax = vmax
+    else:
+        vmax = np.max(img)
+    ax.imshow(img, cmap="Greys", vmin=vmin, vmax=vmax)
+    for orientation in ["top", "bottom", "left", "right"]:
+        ax.spines[orientation].set_visible(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    plt.tight_layout()
+    if savepath != "":
+        plt.savefig(savepath)
+    else:
+        plt.show()
+    plt.clf()
