dda1108ef1c04a35d007b496912969f9dcbdad27
Stephan <s.weissbach@uni-mainz.de>
2024-01-13 00:17:31 +0100

    file writing routine

---
+++ utils/write_file.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tifffile
+
+def write_file(img: np.ndarray[np.uint16], filepath: str) -> None:
+    """
+    Open and read an image file from the specified filepath.
+
+    Parameters:
+    - filepath (str): Path to the image file.
+
+    Returns:
+    - np.ndarray: NumPy array representing the image data.
+
+    Raises:
+    - NotImplementedError: If the file format is not supported.
+
+    Supported file formats:
+    - .nd2 (using nd2reader library)
+    - .tif, .tiff, .stk (using tifffile library)
+    """
+    tiff_fileendings = [".tif", ".tiff", ".stk"]
+
+    if any([filepath.endswith(fileending) for fileending in tiff_fileendings]):
+        tifffile.imwrite(filepath, img)
+    else:
+        raise NotImplementedError(
+            f'Fileformat .{filepath.split(".")[-1]} is currently not implemented. Please change utils/open_file.py'
+        )
